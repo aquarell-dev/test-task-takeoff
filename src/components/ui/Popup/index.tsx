e18d8e10005293993125e392faa5acba09d26ad7
Aquarell <aquarell.dev@gmail.com>
2022-08-14 23:32:54 +0300

    fixed popup closing icong

--- src/components/ui/Popup/index.tsx
+++ src/components/ui/Popup/index.tsx
@@ -20,10 +20,9 @@ const Popup: FC<IPopup> = ({ heading, children, visible, setVisible }) => {
     >
       <div
         className='absolute cursor-pointer left-0 top-0 right-0 bottom-0 flex justify-end mr-2 my-1'
-        onClick={() => setVisible(false)}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-             stroke="currentColor" strokeWidth={2}>
+             stroke="currentColor" strokeWidth={2} onClick={() => setVisible(false)}>
           <path strokeLinecap="round" strokeLinejoin="round"
                 d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
         </svg>
